042c4b2c6e2ae5e924adc27714eab9bc6029606c
FJavohr <115878920+FJavohr@users.noreply.github.com>
2023-04-10 03:36:30 +0600

    basic footer styles

--- client/client-side/src/components/footer/index.jsx
+++ client/client-side/src/components/footer/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 import './Footer.scss'
+import ContentWrapper from '../contentWrapper/ContentWrapper'
 // импортировать иконки соц. сетей.
-
+import {
+FaInstagram,
+FaTwitter,
+FaLinkedin,
+FaFacebookF
+} from 'react-icons/fa'
 //
-
-
-
-
 
 const Footer = () => {
   return (
     <footer className="footer">
-    <ul className="menuItems">
+    <ContentWrapper>
+      <ul className="menuItems">
       <li className="menuItem">SMTH</li>
       <li className="menuItem">SMTH</li>
       <li className="menuItem">SMTH</li>
@@ -21,6 +24,13 @@ const Footer = () => {
     <div className="infoText">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur adipisci repudiandae sapiente, obcaecati soluta recusandae eum, omnis quae error temporibus necessitatibus reiciendis doloribus natus aspernatur vel veniam deleniti ipsum eaque hic itaque doloremque. Beatae minus ducimus vero numquam velit consequuntur saepe ipsam accusamus amet, quis fuga quasi nostrum a sit adipisci voluptates quas illum, inventore, esse enim porro? 
     </div>
+    <div className="socialIcons">
+      <span className='icon'><FaInstagram/></span>
+      <span className='icon'><FaTwitter/></span>
+      <span className='icon'><FaLinkedin/></span>
+      <span className='icon'><FaFacebookF/></span>
+    </div>
+    </ContentWrapper>
     </footer>
   )
 }
