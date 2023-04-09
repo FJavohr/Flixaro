2bd22249c72fed1c52e20a5eeea54ed883ad8804
FJavohr <115878920+FJavohr@users.noreply.github.com>
2023-04-10 03:48:41 +0600

    popular block start

--- client/client-side/src/pages/home/Home.jsx
+++ client/client-side/src/pages/home/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './Home.scss' 
 
 import HeroBanner from './heroBanner/HeroBanner'
-
+import Popular from './popular/Popular'
 
 const Home = () => {
   return (
     <div className="homePage">
       <HeroBanner/>
+      <Popular/>
     </div>
   )
 }
---
+++ client/client-side/src/pages/home/popular/Popular.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Popular.scss'
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
+
+
+const Popular = () => {
+
+  return (
+    <div>hurma</div>
+  )
+}
+
+export default Popular
