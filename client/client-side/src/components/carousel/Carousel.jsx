7a00d77af1dddebfe4ad78b984cf039dce843f55
FJavohr <115878920+FJavohr@users.noreply.github.com>
2023-04-11 00:22:28 +0600

    i added Carousel and Skeleton preload for cards in Carousel

--- client/client-side/src/components/carousel/Carousel.jsx
+++ client/client-side/src/components/carousel/Carousel.jsx
@@ -1,25 +1,40 @@
 import React, { useRef } from 'react'
+import dayjs from 'dayjs';
 import {
   BsFillArrowLeftCircleFill,
   BsFillArrowRightCircleFill,
 } from 'react-icons/bs'
-import backfall from '../../assets/backfall.png'
-import './Carousel.scss'
 import { useNavigate } from 'react-router-dom'
+import './Carousel.scss'
 import { useSelector } from 'react-redux'
-import dayjs from 'dayjs';
-import ContentWrapper from '../contentWrapper/ContentWrapper'
-import Img from '../lazyLoadImage/img'
 //заимпортировать изображение, которое будет появлятся при необнаружении в предоставленных данных
+import Img from '../lazyLoadImage/img'
+import backfall from '../../assets/backfall.png'
+import ContentWrapper from '../contentWrapper/ContentWrapper'
+// import CircleRating from "../circleRating/CircleRating";
 
 const Corousel = ({data, loading}) => {
   const caroselContainer = useRef();
   const {url} = useSelector((state) => state.home)
+  // console.log(url)
   const navigate = useNavigate();
   // console.log(caroselContainer); 
   console.log(data)
   const navigation = (dir) => { 
+
   } 
+
+    const skItem = () => {
+      return (
+        <div className='skeletonItem' >
+          <div className="posterBlock"></div>
+          <div className="textBlock">
+            <div className="title skeleton "></div>
+            <div className="date skeleton "></div>
+          </div>
+        </div>
+      )
+    }
 
   return (
     <div className='carousel'> 
@@ -35,12 +50,21 @@ const Corousel = ({data, loading}) => {
     {!loading ? 
     (
       <div className="carouselItems">
-        {data?.map(({id, poster_path}) => {
+        {data?.map(({id, poster_path, title, name, release_Date }) => {
           const posterUrl = poster_path ? url.poster + poster_path : (<div>U have some problems</div>)
+          console.log(posterUrl)
           return (
             <div className="carouselItem" key={id}>
               <div className="posterBlock">
                 <Img src={posterUrl} />
+              </div>
+              <div className="textBlock">
+                <span className="title">
+                  {title || name}
+                </span>
+                <span className="date">
+                  {dayjs(release_Date).format("MMM D, YYYY")}
+                </span>
               </div>
             </div>
           )
@@ -49,7 +73,14 @@ const Corousel = ({data, loading}) => {
     ) 
     :
     (
-      <span>Laoding .....................</span>
+     <div className="loadingSkeleton">
+      {skItem()}
+      {skItem()}
+      {skItem()}
+      {skItem()}
+      {skItem()}
+      {skItem()}
+     </div>
     )
      }
     </ContentWrapper>
